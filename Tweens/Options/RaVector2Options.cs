f28fdced0083bf77f247c5cf33b4f017c25ddafa
Ramses Di Perna <ramses@sparklingsociety.net>
2022-07-24 17:46:02 +0200

    Implementing UI RectTransform Tweening: * Implemented RaTweenSizeDelta tweening

---
+++ Tweens/Options/RaVector2Options.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+namespace RaTweening
+{
+	public static class RaVector2Options
+	{
+		#region Public Methods
+
+		public static Axis GetOnlyIncludeAxes(Axis inclAxis)
+		{
+			Axis returnValue = Axis.None;
+
+			if(!inclAxis.HasFlag(Axis.X))
+			{
+				returnValue |= Axis.X;
+			}
+
+			if(!inclAxis.HasFlag(Axis.Y))
+			{
+				returnValue |= Axis.Y;
+			}
+
+			return returnValue;
+		}
+
+		public static Vector2 ApplyExcludeAxes(Vector2 original, Vector2 final, Axis excludeAxes)
+		{
+			if(excludeAxes.HasFlag(Axis.X))
+			{
+				final.x = original.x;
+			}
+
+			if(excludeAxes.HasFlag(Axis.Y))
+			{
+				final.y = original.y;
+			}
+
+			return final;
+		}
+
+		#endregion
+
+
+		#region Nested
+
+		[Flags]
+		public enum Axis
+		{
+			None = 0,
+			X = 1,
+			Y = 2,
+			All = X | Y
+		}
+
+		#endregion
+	}
+}
---
+++ Tweens/UI/RectTransform/RaTweenSizeDelta.cs
@@ -0,0 +1,129 @@
+﻿using RaTweening.UI.RaRectTransform;
+using UnityEngine;
+using static RaTweening.RaVector2Options;
+
+namespace RaTweening.UI.RaRectTransform
+{
+	public class RaTweenSizeDelta : RaTweenDynamic<RectTransform, Vector2>
+	{
+		#region Editor Variables
+
+		[Header("RaTweenSizeDelta")]
+		[SerializeField]
+		private Axis _excludeAxis = Axis.None;
+
+		#endregion
+
+		public RaTweenSizeDelta()
+			: base()
+		{
+
+		}
+
+		public RaTweenSizeDelta(RectTransform target, Vector2 startSize, Vector2 endSize, float duration)
+			: base(target, startSize, endSize, duration)
+		{
+
+		}
+
+		public RaTweenSizeDelta(RectTransform target, Vector2 endSize, float duration)
+			: base(target, endSize, duration)
+		{
+
+		}
+
+		public RaTweenSizeDelta(RectTransform target, Vector2 startSize, RectTransform endSize, float duration)
+			: base(target, startSize, default, duration)
+		{
+			SetEndRef(endSize);
+		}
+
+		#region Public Methods
+
+		public RaTweenSizeDelta SetExcludeAxis(Axis excludeAxis)
+		{
+			if(CanBeModified())
+			{
+				_excludeAxis = excludeAxis;
+			}
+			return this;
+		}
+
+		public RaTweenSizeDelta OnlyIncludeAxis(Axis inclAxis)
+		{
+			if(CanBeModified())
+			{
+				_excludeAxis = GetOnlyIncludeAxes(inclAxis);
+			}
+			return this;
+		}
+
+		#endregion
+
+		#region Protected Methods
+
+		protected override RaTweenDynamic<RectTransform, Vector2> DynamicClone()
+		{
+			RaTweenSizeDelta tween = new RaTweenSizeDelta();
+			tween._excludeAxis = _excludeAxis;
+			return tween;
+		}
+
+		protected override void DynamicEvaluation(float normalizedValue, RectTransform target, Vector2 start, Vector2 end)
+		{
+			Vector2 delta = end - start;
+			target.sizeDelta = ApplyExcludeAxes(target.sizeDelta, start + (delta * normalizedValue), _excludeAxis);
+		}
+
+		protected override Vector2 GetEndByDelta(Vector2 start, Vector2 delta)
+		{
+			return start + delta;
+		}
+
+		protected override Vector2 ReadValue(RectTransform reference)
+		{
+			return reference.sizeDelta;
+		}
+
+		#endregion
+	}
+}
+
+namespace RaTweening
+{
+	#region Extensions
+
+	public static partial class RaTweenUtilExtensions
+	{
+		public static RaTweenSizeDelta TweenSizeWidth(this RectTransform self, float width, float duration)
+		{
+			return new RaTweenSizeDelta(self, Vector2.one * width, duration)
+				.OnlyIncludeAxis(Axis.X)
+				.Play();
+		}
+
+		public static RaTweenSizeDelta TweenSizeHeight(this RectTransform self, float height, float duration)
+		{
+			return new RaTweenSizeDelta(self, Vector2.one * height, duration)
+				.OnlyIncludeAxis(Axis.Y)
+				.Play();
+		}
+
+		public static RaTweenSizeDelta TweenSize(this RectTransform self, Vector3 size, float duration)
+		{
+			return new RaTweenSizeDelta(self, size, duration).Play();
+		}
+
+		public static RaTweenSizeDelta TweenSize(this RectTransform self, Vector3 startSize, Vector3 endSize, float duration)
+		{
+			return new RaTweenSizeDelta(self, startSize, endSize, duration).Play();
+		}
+
+		public static RaTweenSizeDelta TweenSize(this RectTransform self, Vector3 startPos, RectTransform endTarget, float duration)
+		{
+			return new RaTweenSizeDelta(self, startPos, endTarget, duration).Play();
+		}
+	}
+
+	#endregion
+}
