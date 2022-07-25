2348f424717930107d676717c987f075aaf3bb4a
Ramses Di Perna <ramses@sparklingsociety.net>
2022-07-25 22:13:04 +0200

    Added Shadow Tweening Support

---
+++ Tweens/UI/Shadow/RaTweenColor.cs
@@ -0,0 +1,137 @@
+﻿using RaTweening.UI.RaShadow;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using static RaTweening.RaColorOptions;
+
+namespace RaTweening.UI.RaShadow
+{
+	[Serializable]
+	public class RaTweenColor : RaTweenDynamic<Shadow, Color>
+	{
+		#region Editor Variables
+
+		[Header("RaTweenColor")]
+		[SerializeField]
+		private Channel _excludeChannels = Channel.None;
+
+		#endregion
+
+		public RaTweenColor()
+			: base()
+		{
+
+		}
+
+		public RaTweenColor(Shadow target, Color startColor, Color endColor, float duration)
+			: base(target, startColor, endColor, duration)
+		{
+
+		}
+
+		public RaTweenColor(Shadow target, Color endColor, float duration)
+			: base(target, endColor, duration)
+		{
+
+		}
+
+		#region Public Methods
+
+		public RaTweenColor SetExcludeChannels(Channel excludeChannels)
+		{
+			if(CanBeModified())
+			{
+				_excludeChannels = excludeChannels;
+			}
+			return this;
+		}
+
+		public RaTweenColor OnlyIncludeChannels(Channel inclChannels)
+		{
+			if(CanBeModified())
+			{
+				_excludeChannels = GetOnlyIncludeChannels(inclChannels);
+			}
+			return this;
+		}
+
+		#endregion
+
+		#region Protected Methods
+
+		protected override void SetDefaultValues()
+		{
+			base.SetDefaultValues();
+			SetStartValue(Color.white);
+			SetEndValue(Color.white);
+		}
+
+		protected override void DynamicEvaluation(float normalizedValue, Shadow target, Color start, Color end)
+		{
+			Color delta = end - start;
+			target.effectColor = ApplyExcludeChannels(target.effectColor, start + (delta * normalizedValue), _excludeChannels);
+		}
+
+		protected override RaTweenDynamic<Shadow, Color> DynamicClone()
+		{
+			RaTweenColor tween = new RaTweenColor();
+			tween._excludeChannels = _excludeChannels;
+			return tween;
+		}
+
+		protected override Color ReadValue(Shadow reference)
+		{
+			return reference.effectColor;
+		}
+
+		protected override Color GetEndByDelta(Color start, Color delta)
+		{
+			return start + delta;
+		}
+
+		#endregion
+	}
+}
+
+namespace RaTweening
+{
+	#region Extensions
+
+	public static partial class RaTweenUtilExtensions
+	{
+		public static RaTweenColor TweenColorR(this Shadow self, float red, float duration)
+		{
+			return new RaTweenColor(self, Color.white * red, duration)
+				.OnlyIncludeChannels(Channel.R)
+				.Play();
+		}
+
+		public static RaTweenColor TweenColorG(this Shadow self, float green, float duration)
+		{
+			return new RaTweenColor(self, Color.white * green, duration)
+				.OnlyIncludeChannels(Channel.G)
+				.Play();
+		}
+
+		public static RaTweenColor TweenColorB(this Shadow self, float blue, float duration)
+		{
+			return new RaTweenColor(self, Color.white * blue, duration)
+				.OnlyIncludeChannels(Channel.B)
+				.Play();
+		}
+
+		public static RaTweenColor TweenColorA(this Shadow self, float alpha, float duration)
+		{
+			return new RaTweenColor(self, Color.white * alpha, duration)
+				.OnlyIncludeChannels(Channel.A)
+				.Play();
+		}
+
+		public static RaTweenColor TweenColor(this Shadow self, Color color, float duration)
+		{
+			return new RaTweenColor(self, color, duration).Play();
+		}
+	}
+
+	#endregion
+}
---
+++ Tweens/UI/Shadow/RaTweenSizeDelta.cs
@@ -0,0 +1,130 @@
+﻿using RaTweening.UI.RaShadow;
+using UnityEngine;
+using UnityEngine.UI;
+using static RaTweening.RaVector2Options;
+
+namespace RaTweening.UI.RaShadow
+{
+	public class RaTweenDistance : RaTweenDynamic<Shadow, Vector2>
+	{
+		#region Editor Variables
+
+		[Header("RaTweenDistance")]
+		[SerializeField]
+		private Axis _excludeAxis = Axis.None;
+
+		#endregion
+
+		public RaTweenDistance()
+			: base()
+		{
+
+		}
+
+		public RaTweenDistance(Shadow target, Vector2 startDistance, Vector2 endDistance, float duration)
+			: base(target, startDistance, endDistance, duration)
+		{
+
+		}
+
+		public RaTweenDistance(Shadow target, Vector2 endDistance, float duration)
+			: base(target, endDistance, duration)
+		{
+
+		}
+
+		public RaTweenDistance(Shadow target, Vector2 startDistance, Shadow endDistance, float duration)
+			: base(target, startDistance, default, duration)
+		{
+			SetEndRef(endDistance);
+		}
+
+		#region Public Methods
+
+		public RaTweenDistance SetExcludeAxis(Axis excludeAxis)
+		{
+			if(CanBeModified())
+			{
+				_excludeAxis = excludeAxis;
+			}
+			return this;
+		}
+
+		public RaTweenDistance OnlyIncludeAxis(Axis inclAxis)
+		{
+			if(CanBeModified())
+			{
+				_excludeAxis = GetOnlyIncludeAxes(inclAxis);
+			}
+			return this;
+		}
+
+		#endregion
+
+		#region Protected Methods
+
+		protected override RaTweenDynamic<Shadow, Vector2> DynamicClone()
+		{
+			RaTweenDistance tween = new RaTweenDistance();
+			tween._excludeAxis = _excludeAxis;
+			return tween;
+		}
+
+		protected override void DynamicEvaluation(float normalizedValue, Shadow target, Vector2 start, Vector2 end)
+		{
+			Vector2 delta = end - start;
+			target.effectDistance = ApplyExcludeAxes(target.effectDistance, start + (delta * normalizedValue), _excludeAxis);
+		}
+
+		protected override Vector2 GetEndByDelta(Vector2 start, Vector2 delta)
+		{
+			return start + delta;
+		}
+
+		protected override Vector2 ReadValue(Shadow reference)
+		{
+			return reference.effectDistance;
+		}
+
+		#endregion
+	}
+}
+
+namespace RaTweening
+{
+	#region Extensions
+
+	public static partial class RaTweenUtilExtensions
+	{
+		public static RaTweenDistance TweenDistanceX(this Shadow self, float distanceX, float duration)
+		{
+			return new RaTweenDistance(self, Vector2.one * distanceX, duration)
+				.OnlyIncludeAxis(Axis.X)
+				.Play();
+		}
+
+		public static RaTweenDistance TweenDistanceY(this Shadow self, float distanceY, float duration)
+		{
+			return new RaTweenDistance(self, Vector2.one * distanceY, duration)
+				.OnlyIncludeAxis(Axis.Y)
+				.Play();
+		}
+
+		public static RaTweenDistance TweenDistance(this Shadow self, Vector2 distance, float duration)
+		{
+			return new RaTweenDistance(self, distance, duration).Play();
+		}
+
+		public static RaTweenDistance TweenDistance(this Shadow self, Vector2 startDistance, Vector2 endDistance, float duration)
+		{
+			return new RaTweenDistance(self, startDistance, endDistance, duration).Play();
+		}
+
+		public static RaTweenDistance TweenDistance(this Shadow self, Vector2 startDistance, Shadow endTarget, float duration)
+		{
+			return new RaTweenDistance(self, startDistance, endTarget, duration).Play();
+		}
+	}
+
+	#endregion
+}
